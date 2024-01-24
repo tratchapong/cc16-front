693c7d8338d944b8c59088240a3eddfaaf8571ac
tratchapong <tratchapong@gmail.com>
2024-01-24 23:21:25 +0700

    starter with tailwind + typography + eslintrc

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <div className="prose">
+    <h1 className="underline">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Noto Sans Thai', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
