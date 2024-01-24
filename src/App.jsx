6844346ec7304c766d6cd05768a289008c94ef50
tratchapong <tratchapong@gmail.com>
2024-01-24 23:55:01 +0700

    add Theme toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 function App() {
+  const [toggle, setToggle] = useState(false);
+
+  const hdlToggle = () => {
+    setToggle(prv=>!prv)
+  }
   return (
-    <div className="prose">
-      <h1 className="underline">Hello world!</h1>
-      <div>
+    <div className="w-screen h-screen flex flex-col gap-3" data-theme={toggle ? 'dark': ''}>
+      <div className="prose">
+        <h1 className="underline">Hello world!</h1>
+      </div>
+      <div className="form-control">
+        <input type="checkbox" className="toggle" checked={toggle} onChange={hdlToggle} />
+      </div>
+      <div className="flex gap-1">
         <button className="btn">Button</button>
         <button className="btn btn-neutral">Neutral</button>
         <button className="btn btn-primary">Primary</button>
