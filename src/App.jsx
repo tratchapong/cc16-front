e0f72ab309a4a5a74a6bb54ec758b04873a4d006
tratchapong <tratchapong@gmail.com>
2024-01-24 23:30:34 +0700

    add DaisyUI plugin

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-
 function App() {
-
   return (
     <div className="prose">
-    <h1 className="underline">
-      Hello world!
-    </h1>
+      <h1 className="underline">Hello world!</h1>
+      <div>
+        <button className="btn">Button</button>
+        <button className="btn btn-neutral">Neutral</button>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-accent">Accent</button>
+        <button className="btn btn-ghost">Ghost</button>
+        <button className="btn btn-link">Link</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
